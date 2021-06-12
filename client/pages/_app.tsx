c3cc6a3d5198b14df452b778fadc51566f2c7338
restuwahyu13 <restuwahyu13@gmail.com>
2021-06-13 01:12:16 +0700

    fixed error

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,13 +1,20 @@
 // CSS Module Teritory
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../../public/static/css/style.css'
-import { Head } from 'next/document'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }) {
 	return (
 		<>
 			<Head>
 				<title>Minimalize Modern Next Boilerplate</title>
+				<meta charSet='utf-8' />
+				<meta httpEquiv='X-UA-Compatible' content='IE=edge' />
+				<meta name='viewport' content='width=device-width, initial-scale=1' />
+				<meta name='theme-color' content='#000000' />
+				<meta name='keywords' content='Nextjs CSA Boilerplate' />
+				<meta name='description' content='Minimalize Modern Next Boilerplate 2021' />
+				<link rel='stylesheet' href='public/static/css/style.css' type='text/css' />
 			</Head>
 			<Component {...pageProps} />
 		</>
--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -4,15 +4,7 @@ export default class RootDocument extends Document {
   render() {
     return (
       <Html>
-        <Head>
-          <meta charSet='utf-8' />
-          <meta http-equiv='X-UA-Compatible' content='IE=edge' />
-          <meta name='viewport' content='width=device-width, initial-scale=1' />
-          <meta name='theme-color' content='#000000' />
-          <meta name='keywords' content='Nextjs CSA Boilerplate' />
-          <meta name='description' content='Minimalize Modern Next Boilerplate 2021' />
-          <link rel='stylesheet' href='public/static/css/style.css' type='text/css' />
-        </Head>
+        <Head />
         <body>
           <Main />
           <NextScript />
