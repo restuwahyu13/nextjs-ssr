6884909ab5a5c40bad28afbd60237ed539cd7dd7
restuwahyu13 <restuwahyu13@gmail.com>
2021-06-13 00:55:09 +0700

    fixed meta data error

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,7 +1,15 @@
 // CSS Module Teritory
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../../public/static/css/style.css'
+import { Head } from 'next/document'
 
 export default function App({ Component, pageProps }) {
-	return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>Minimalize Modern Next Boilerplate</title>
+			</Head>
+			<Component {...pageProps} />
+		</>
+	)
 }
--- client/pages/_document.tsx
+++ client/pages/_document.tsx
@@ -1,18 +1,17 @@
-import Document, {Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document'
 
 export default class RootDocument extends Document {
   render() {
     return (
       <Html>
-          <Head>
-          <title>Minimalize Modern Next Boilerplate</title>
+        <Head>
           <meta charSet='utf-8' />
           <meta http-equiv='X-UA-Compatible' content='IE=edge' />
           <meta name='viewport' content='width=device-width, initial-scale=1' />
           <meta name='theme-color' content='#000000' />
           <meta name='keywords' content='Nextjs CSA Boilerplate' />
           <meta name='description' content='Minimalize Modern Next Boilerplate 2021' />
-          <link rel="stylesheet" href="public/static/css/style.css" type="text/css" />
+          <link rel='stylesheet' href='public/static/css/style.css' type='text/css' />
         </Head>
         <body>
           <Main />
